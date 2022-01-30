f423e0236f9c7c462d515fe03470df4e8ce974a1
mayanksingh2233 <singhmayank.ms123@gmail.com>
2022-01-30 18:12:18 +0530

    Updates

---
+++ precache-manifest.90321c78108eec3ae07d2ed4cf93c649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79af278e504ebc307be4f5a8356bb4f7",
+    "url": "/Trello-clone/index.html"
+  },
+  {
+    "revision": "deae75990b08ed5a5e41",
+    "url": "/Trello-clone/static/css/main.2230cac3.chunk.css"
+  },
+  {
+    "revision": "f5580e9e7bdd5aca7d61",
+    "url": "/Trello-clone/static/js/2.eed53e34.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Trello-clone/static/js/2.eed53e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deae75990b08ed5a5e41",
+    "url": "/Trello-clone/static/js/main.72307bb0.chunk.js"
+  },
+  {
+    "revision": "ea0be6177e92c95bc253",
+    "url": "/Trello-clone/static/js/runtime-main.44c2e931.js"
+  },
+  {
+    "revision": "326181e964aa4eb489e83e66a584444b",
+    "url": "/Trello-clone/static/media/bg.326181e9.jpg"
+  }
+]);
